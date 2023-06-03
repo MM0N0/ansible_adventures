54edf5b41aca9682ed4f90267ee45b6bcae604ad
Your Name <you@example.com>
2023-06-03 18:29:35 +0200

    update simple testsetup

--- .test/simple_test/test.sh
+++ .test/simple_test/test.sh
@@ -6,9 +6,12 @@ set -o pipefail
 
 ./.test/docker/clean_centos/up.sh
 
-ansible-playbook -i hosts/dev/ -i user_vault.yml playbooks/testing/run_cmd_remote.yml
-ansible-playbook -i hosts/dev/ -i user_vault.yml playbooks/testing/simple_test/test-role.yml
+ansible-playbook -i hosts/dev/ -i user_vault.yml \
+ playbooks/testing/run_cmd_local.yml \
+ playbooks/testing/run_cmd_remote.yml \
+ playbooks/testing/simple_test/test-role.yml \
+ playbooks/testing/simple_test/run_cmd_remote_show_files.yml
 
-ansible-playbook -i hosts/dev/ -i user_vault.yml playbooks/testing/simple_test/run_cmd_remote_show_files.yml
+ansible-playbook -i hosts/dev/ -i user_vault.yml playbooks/testing/simple_test/test-role.yml
 
 ./.test/docker/clean_centos/down.sh
