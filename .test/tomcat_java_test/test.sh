7bf44133b94236ac8efda926fb9220288fd59258
Your Name <you@example.com>
2023-06-04 13:26:40 +0200

    update tomcat and java test; split root- from non-root-tasks

--- .test/tomcat_java_test/test.sh
+++ .test/tomcat_java_test/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+./.test/docker/clean_centos/down.sh
 
 # to make script abort, when any command fails
 set -e
@@ -6,11 +7,18 @@ set -o pipefail
 
 ./.test/docker/clean_centos/up.sh
 
+# do root tasks
 ansible-playbook -i hosts/dev/ -i user_vault.yml \
  playbooks/testing/java_test/java-role.yml \
  playbooks/testing/tomcat_test/tomcat-role.yml \
- -e "can_become_root=true"
+ -e "can_become_root=true" --tags root_tasks
 
-curl localhost:8080
+# do non-root tasks
+# (can_become_root is set to false in user_vault.yml)
+ansible-playbook -i hosts/dev/ -i user_vault.yml \
+ playbooks/testing/java_test/java-role.yml \
+ playbooks/testing/tomcat_test/tomcat-role.yml
+
+sleep 2 && curl localhost:8080
 
 ./.test/docker/clean_centos/down.sh
