dfc52a307c0d2508f06fcb821af15e0fd72c3d67
Your Name <you@example.com>
2023-06-06 19:44:04 +0200

    update test_master workflow

--- .test/docker/clean_centos/up.sh
+++ .test/docker/clean_centos/up.sh
@@ -4,5 +4,5 @@
 SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
 docker build -t centos_base "$SCRIPT_DIR"
 # start
-docker run --rm --privileged -d -t -p 23:22 -p 8090:8090 -p 8080:8080 \
+docker run --rm --privileged -d -t -p 22:22 -p 8090:8090 -p 8080:8080 \
   --name=centos_base_container --rm centos_base
