1e24eefb6e84255e89c26d63ed864c4cbdc8ccb4
Your Name <you@example.com>
2023-06-06 19:34:02 +0200

    update test_master workflow

--- .test/simple_test/test.sh
+++ .test/simple_test/test.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 ./.test/docker/clean_centos/up.sh
 
-ansible-playbook -i hosts/dev/ -i user_vault.yml \
+ansible-playbook -vvv -i hosts/dev/ -i user_vault.yml \
  playbooks/testing/run_cmd_local.yml \
  playbooks/testing/run_cmd_remote.yml \
  playbooks/testing/simple_test/test-role.yml \
